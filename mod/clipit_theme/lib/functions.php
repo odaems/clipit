ace85a10a976ecfe26cc14b7f65ee0d9b614205f
magutierrez <magutierrezmoreno@gmail.com>
2014-02-19 17:27:15 +0100

    group_events

--- mod/clipit_theme/lib/functions.php
+++ mod/clipit_theme/lib/functions.php
@@ -7,35 +7,104 @@
  * To change this template use File | Settings | File Templates.
  */
 
-function activity_color(){
+function clipit_student_events($rel_array){
+    $content = "";
+    foreach($rel_array as $relationship){
+        $subtype = $relationship->object_subtype;
+        $object_rel = get_relationship($relationship->object_id);
+        $general_subtype = explode("-", $subtype);
+        $general_subtype = (string)$general_subtype[0];
+        // Group, Activity
+        switch($general_subtype){
+            case 'group':
+                $content .= group_events($subtype, $object_rel, $relationship);
+                break;
+            case 'activity':
+                $content .= activity_events($subtype, $object_rel, $relationship);
+                break;
+        }
 
+    }
+    return $content;
 }
 
-// Inutilizado
-function OLD_add_landing_tool_option($name, $label, $view, $default_on = true) {
-    global $CONFIG;
+/*
+ * Group events types
+ */
+function group_events($subtype, $object_rel, $relationship){
+    $content = "";
+    // Activity object
+    $activity_id = ClipitGroup::get_activity($object_rel->guid_one);
+    $activity = array_pop(ClipitActivity::get_by_id(array($activity_id)));
+    // Group object
+    $group = array_pop(ClipitGroup::get_by_id(array($object_rel->guid_one)));
+    // User object
+    $user = array_pop(ClipitUser::get_by_id(array($relationship->performed_by_guid)));
+    $user = new ElggUser($user->id);
+    switch($subtype){
+        case "group-user":
+            $items = array(
+                'item' => array_pop(ClipitGroup::get_by_id(array($object_rel->guid_one))),
+                'sub-item' => array_pop(ClipitUser::get_by_id(array($relationship->performed_by_guid))),
+            );
 
-    if (!isset($CONFIG->landing_tool_options)) {
-        $CONFIG->landing_tool_options = array();
+            $params =  array(
+                'title' => 'Nuevo miembro',
+                'href'  => 'profile/'.$items['sub-item']->login,
+                'icon'  => 'user',
+                'color' => $activity->color,
+                'time'  => $relationship->time_created,
+                'details' => array(
+                    'img'   => elgg_view('output/img', array(
+                        'src' => $user->getIconURL('small'),
+                        'alt' => $user->name,
+                        'title' => elgg_echo('profile'),
+                        'class' => 'img-thumb',
+                    )),
+                    'title' => $user->name,
+                    'sub-title' => 'En '.$group->name,
+                ),
+            );
+
+//            $params =  array(
+//                'title' => 'Nuevo miembro',
+//                'icon' => 'user',
+//                'href'  => 'profile/'.$items['sub-item']->login,
+//                'color' => $activity->color,
+//                'content' => elgg_view("events/group_user", $items), // object user+group info
+//                'time'  => $relationship->time_created
+//            );
+            $content .= elgg_view("page/components/timeline_event", $params);
+            break;
+        case "group-file":
+            $items = array(
+                'item' => array_pop(ClipitFile::get_by_id(array($object_rel->guid_two))),
+                'sub-item' => array_pop(ClipitUser::get_by_id(array($relationship->performed_by_guid))),
+            );
+            $file = array_pop(ClipitFile::get_by_id(array($object_rel->guid_two)));
+            $params =  array(
+                'title' => 'Archivo subido',
+                'href'  => 'clipit_activity/'.$activity->id.'/file/'.$file->id,
+                'icon'  => 'file-text',
+                'color' => $activity->color,
+                'time'  => $relationship->time_created,
+                'details' => array(
+                    'icon'   => 'file-text',
+                    'title' => $file->name,
+                    'sub-title' => 'By '.$user->name,
+                    'description' => $file->description,
+                ),
+            );
+//            $params =  array(
+//                'title' => 'Archivo subido',
+//                'icon' => 'file-text',
+//                'href'  => 'clipit_activity/'.$activity->id.'/file/'.$items['item']->id,
+//                'color' => $activity->color,
+//                'content' => elgg_view("events/group_file", $items), // object user+group info
+//                'time'  => $relationship->time_created
+//            );
+            $content .= elgg_view("page/components/timeline_event", $params);
+            break;
     }
-
-    $landing_tool_option = new stdClass;
-
-    $landing_tool_option->name = $name;
-    $landing_tool_option->label = $label;
-    $landing_tool_option->view = $view;
-    $landing_tool_option->default_on = $default_on;
-
-    $CONFIG->landing_tool_options[] = $landing_tool_option;
-}
-
-
-function add_landing_tool_option($context, array $options) {
-    global $CONFIG;
-
-    if (!isset($CONFIG->landing_tool_options)) {
-        $CONFIG->landing_tool_options = array();
-    }
-
-    $CONFIG->landing_tool_options[$context][] = $options;
+    return $content;
 }
